e3c03eca9fb9ac3a972dc852605c4f0ffe17c01a
hyunjun82 <33han58@gmail.com>
2026-06-25 17:07:05 +0900

    Delete app/robots.ts

--- app/robots.ts
+++
@@ -1,9 +0,0 @@
-import type { MetadataRoute } from "next";
-import { SITE } from "@/lib/sites";
-
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: { userAgent: "*", allow: "/" },
-    sitemap: `${SITE.baseUrl}/sitemap.xml`,
-  };
-}
